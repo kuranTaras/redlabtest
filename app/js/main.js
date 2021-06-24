5328edb2a8d1b70e54c9c89171b411b8d73ef45e
kuranTaras <rebystaras@gmail.com>
2021-06-24 15:11:29 +0300

    1.1

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,23 @@
 $(window).on("load" , function () {
-    $(window).on('scroll', () => {
-        let top = $('.services').offset().top / 2
-        if ($(window).scrollTop() > top) {
-            $('.services__item').addClass('active')
-        }
-    })
+
 
 
 
 })
+$(window).on('scroll', () => {
+    let top = $('.services').offset().top / 2
+    if ($(window).scrollTop() > top) {
+        $('.services__item').addClass('active')
+    }
 
+
+
+
+    if ($(window).scrollTop() > 100) {
+        $('.order').addClass('order_active')
+    } else {
+        $('.order').removeClass('order_active')
+    }
+})
 
 
