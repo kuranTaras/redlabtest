a7aa4d891fa1403ed62d7acb47c1fa3a17aa9f2b
kuranTaras <rebystaras@gmail.com>
2021-06-25 14:16:47 +0300

    1.7

--- app/js/app.min.js
+++ app/js/app.min.js
@@ -5,7 +5,7 @@ $(window).on("load" , function () {
         const position = example.getBoundingClientRect();
         const x = e.clientX - position.left - position.width / 2;
         const y = e.clientY - position.top - position.height / 1.35 ;
-        $('.order__button').css('transform', 'translate(' + x * 0.5 + 'px' + ',' + y * 0.3 + 'px' + ') scale(1)');
+        $('.order__button').css('transform', 'translate(' + x * 0.7 + 'px' + ',' + y * 0.3 + 'px' + ') scale(1)');
         $('.order__button').addClass('order_hover')
     });
     $('.order').on('mouseleave', (e) => {
@@ -24,10 +24,6 @@ $(window).on('scroll', () => {
     if ($(window).scrollTop() > top) {
         $('.services__item').addClass('active')
     }
-
-
-
-
     if ($(window).scrollTop() > 100) {
         $('.order__button').css('transform', 'scale(0.8)')
         $('.order').addClass('order_active')
@@ -35,5 +31,22 @@ $(window).on('scroll', () => {
         $('.order__button').css('transform', 'scale(1)')
         $('.order').removeClass('order_active')
     }
+    if ($(window).scrollTop() === 0) {
+        $('.header').removeClass('header_active')
+    }
+})
+
+$(window).on('mousewheel', (e) => {
+    if ($(window).scrollTop() > 70) {
+        if(e.originalEvent.wheelDelta > 0) {
+            $('.header').addClass('header_active')
+            $('.header').removeClass('header_hide')
+        }
+        if(e.originalEvent.wheelDelta < 0) {
+            $('.header').addClass('header_hide')
+        }
+    }
+
+
 })
 
